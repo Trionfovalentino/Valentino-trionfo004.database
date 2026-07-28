93f926ffabf4ee862fd9acf12ac14c8690622e56
Valentino <trionfovalentino200@otlook.it>
2026-07-28 16:12:53 +0200

    Fix: rimossa cartella OneDrive da GitHub

--- OneDrive/Desktop/laravel_05_valentino-trionfo/laravel_05_valentino_trionfo/resources/views/welcome.blade.php
+++
@@ -1,7 +0,0 @@
-<x-layout title="Homepage - TechBlog">
-    <div class="p-5 mb-4 bg-white rounded-3 shadow-sm text-center">
-        <h1 class="display-4 fw-bold">Benvenuto su TechBlog</h1>
-        <p class="lead">Progetto con invio email gestito tramite Controller, Mailable e Mailtrap.</p>
-        <a href="{{ route('contact.form') }}" class="btn btn-primary btn-lg mt-3">Vai al Form di Contatto</a>
-    </div>
-</x-layout>
--- OneDrive/Desktop/laravel_05_valentino-trionfo/laravel_05_valentino_trionfo/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PublicController;
-use App\Http\Controllers\ContactController;
-
-Route::get('/', [PublicController::class, 'home'])->name('homepage');
-Route::get('/contatti', [ContactController::class, 'form'])->name('contact.form');
-Route::post('/contatti/invio', [ContactController::class, 'send'])->name('contact.send');
